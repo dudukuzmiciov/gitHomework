261d2380a289841fc9146e14983d228ab2305e89
DANA CUZMICIOV <dudukuzmiciov@gmail.com>
2022-02-18 23:10:38 -0500

    first commit

---
+++ src/gitHW3/HomeWork.java
@@ -0,0 +1,13 @@
+package gitHW3;
+
+public class HomeWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Dana");
+		System.out.println("New York");
+		System.out.println("Dana live's in NY");
+	}
+
+}
